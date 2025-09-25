1dd89e292382b755d8322643d8579fab2b12ed6d
FelipePedraza <davidf.pedrazab@uqvirtual.edu.co>
2025-09-25 11:08:35 -0500

    revisando integridad

--- src/main/java/co/edu/uniquindio/application/dtos/alojamiento/AlojamientoDTO.java
+++ src/main/java/co/edu/uniquindio/application/dtos/alojamiento/AlojamientoDTO.java
@@ -8,17 +8,24 @@ import java.util.List;
 public record AlojamientoDTO(
         @NotBlank
         String titulo,
+
         @NotBlank
         String descripcion,
+
         @NotNull
-        DireccionDTO ubicacion,
+        DireccionDTO direccion,
+
         @NotNull @Min(0)
-        Float precioNoche,
+        Float precioPorNoche,
+
         @NotNull @Min(1)
-        Integer capacidad,
+        Integer maxHuespedes,
+
         List<Servicio> servicios,
+
         @NotNull @Size(min = 1, max = 10)
         List<String> imagenes,
+
         @Min(0)
         Integer imagenPrincipal
 ) {
