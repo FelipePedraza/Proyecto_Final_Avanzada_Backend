5740e9f999c0c29f8cf9146c935e52e512f3e6d3
FelipePedraza <davidf.pedrazab@uqvirtual.edu.co>
2026-02-27 10:06:31 -0500

    Se agrego la pasarela de pago con el servicio de stripe

--- src/main/java/co/edu/uniquindio/application/dtos/reserva/CreacionReservaRespuestaDTO.java
+++ src/main/java/co/edu/uniquindio/application/dtos/reserva/CreacionReservaRespuestaDTO.java
@@ -1,6 +1,6 @@
 // ─── CreacionReservaRespuestaDTO.java ─────────────────────────────────────────
 // Extiende la respuesta al crear reserva para incluir datos de pago.
-// El frontend recibe todo lo que necesita en una sola llamada.
+// El frontend recibe tdo lo que necesita en una sola llamada.
 package co.edu.uniquindio.application.dtos.reserva;
 
 import co.edu.uniquindio.application.dtos.pago.PagoIntentDTO;
