26826edf05d821a433c7654691b322a9bf6506a2
FelipePedraza <davidf.pedrazab@uqvirtual.edu.co>
2026-03-25 08:28:43 -0500

    se coloco el nuevo dominio del frontend en cors

--- src/main/java/co/edu/uniquindio/application/config/SeguridadConfig.java
+++ src/main/java/co/edu/uniquindio/application/config/SeguridadConfig.java
@@ -54,7 +54,7 @@ public class SeguridadConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         // Configura las políticas de CORS para permitir solicitudes desde el frontend
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:63342", "http://localhost:4200", "https://awsamplify.dlzssnb4fp5n9.amplifyapp.com"));
+        config.setAllowedOrigins(List.of("http://localhost:63342", "http://localhost:4200", "https://awsamplify.d27zb7a8342f4p.amplifyapp.com"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
--- src/main/java/co/edu/uniquindio/application/config/WebSocketConfig.java
+++ src/main/java/co/edu/uniquindio/application/config/WebSocketConfig.java
@@ -24,7 +24,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
                 .addEndpoint("/ws")
                 .setHandshakeHandler(new CustomHandshakeHandler())
                 .addInterceptors(webSocketAuthInterceptor)
-                .setAllowedOrigins("http://localhost:4200", "http://localhost:8080", "http://localhost:63342", "https://awsamplify.dlzssnb4fp5n9.amplifyapp.com") // Punto final del WebSocket
+                .setAllowedOrigins("http://localhost:4200", "http://localhost:8080", "http://localhost:63342", "https://awsamplify.d27zb7a8342f4p.amplifyapp.com") // Punto final del WebSocket
                 .withSockJS();
     }
 
